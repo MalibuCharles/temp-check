8cd47c34c6e8ed12c9fde5b8b88d3c06809c5214
Berthonise Charles <berthonisecharles@Berthonises-MacBook-Air-2.local>
2022-01-21 13:58:41 -0500

    temp check

---
+++ tempature.js
@@ -0,0 +1,8 @@
+let temp = 8.5
+function convertCtoF(cel){
+    const far = (cel * 1.8) + 32
+    return far 
+}
+
+const fahrenheit = convertCtoF(temp);
+console.log(`${temp} degrees celsius is equivalent to ${fahrenheit} degrees fahrenheit`)
